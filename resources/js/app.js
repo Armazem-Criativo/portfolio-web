a9e78f904a81ca1a0939aef99c0cc5ef5f31d622
Gustavo Raulino <gustavoraulinodasilva@gmail.com>
2024-08-18 16:07:44 -0300

    trocar de cor menu portfolio

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+function init(){
+    changeColorHeader();
+}
+
+function changeColorHeader()
+{
+    var headerHeight = $('.header').height();
+    var headerOrPosition = $('.header').offset().top;
+
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > 100) {
+            $('.header').addClass('active');
+        }else{
+            $('.header').removeClass('active');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', init);
--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -27,6 +27,7 @@
     @yield('content')
 
     @include('sections.footer')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </body>
 
 </html>
