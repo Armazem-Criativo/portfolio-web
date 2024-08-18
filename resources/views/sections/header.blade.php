266db7252ac41d9a470a648e107be9fc202119c7
Gustavo Raulino <gustavoraulinodasilva@gmail.com>
2024-08-17 22:35:24 -0300

    Criado o header e o rodape

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,1 +1,19 @@
-<h1>Cabeçalho do site</h1>
+@section('css')
+    <link rel="stylesheet" href="{{ asset('resources/css/sections/header.css') }}">
+@endsection
+
+<header class="header">
+    <nav class="nav">
+        <div class="logo">
+            <a href="/">
+                <p><\Gustavo></p>
+            </a>
+        </div>
+        <ul class="links">
+            <li class="item"><a class="link" href="">Sobre</a></li>
+            <li class="item"><a class="link" href="">Tecnologias</a></li>
+            <li class="item"><a class="link" href="">Projetos</a></li>
+            <li class="item"><a class="link" href="">Contato</a></li>
+        </ul>
+    </nav>
+</header>
