00776a896f624caf977441536dc2e4d55c5eff74
Gustavo Raulino <gustavoraulinodasilva@gmail.com>
2024-10-15 22:05:45 -0300

    Reformulado a seção banner

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,88 @@
-import './bootstrap';
+import "./bootstrap";
 
-function init(){
-    changeColorHeader();
+function init() {
+    // changeColorHeader();
+    particle();
 }
 
-function changeColorHeader()
-{
-    var headerHeight = $('.header').height();
-    var headerOrPosition = $('.header').offset().top;
-
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if(scroll > 100) {
-            $('.header').addClass('active');
-        }else{
-            $('.header').removeClass('active');
-        }
+function particle() {
+    particlesJS("particles-js", {
+        particles: {
+            number: {
+                value: 80,
+            },
+            color: {
+                value: "#007bff",
+            },
+            shape: {
+                type: "circle",
+                stroke: {
+                    width: 0,
+                    color: "#000000",
+                },
+                polygon: {
+                    nb_sides: 5,
+                },
+            },
+            opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                    enable: false,
+                },
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: false,
+                },
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#007bff",
+                opacity: 0.4,
+                width: 1,
+            },
+            move: {
+                enable: true,
+                speed: 2,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                    enable: false,
+                },
+            },
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                onclick: {
+                    enable: true,
+                    mode: "push",
+                },
+                resize: true,
+            },
+            modes: {
+                repulse: {
+                    distance: 100,
+                    duration: 0.4,
+                },
+                push: {
+                    particles_nb: 4,
+                },
+            },
+        },
+        retina_detect: true,
     });
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,9 @@
 @endsection
 
 @section('content')
+    <div id="particles-js"></div>
     <div id="banner" class="banner">
         <div class="content">
-            <div class="image">
-                <img src="{{asset('images/fotodeperfil-removebg-preview.png')}}" alt="">
-            </div>
             <h1 class="present-myself">
                 Olá, Eu sou <strong>Gustavo Raulino da Silva</strong>
             </h1>
@@ -18,24 +16,34 @@
             </h3>
             <div class="about-me">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio enim assumenda, dicta odio rerum beatae.
-                    Libero ad quas, harum illum excepturi iure deserunt, cumque doloremque natus quaerat, facere pariatur
-                    voluptates!</p>
+                </p>
             </div>
             <div class="actions">
+                <a href="" class="btn-action">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                    </svg>
+                    <p>Github</p>
+                </a>
+                <a href="" class="btn-action">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                    </svg>
+                    <p>Linkedin</p>
+                </a>
                 <a href="" class="btn-action">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="bi bi-paperclip" viewBox="0 0 16 16">
                         <path
                             d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z" />
                     </svg>
-                    <p>Curriculo</p>
+                    <p>Currículo</p>
                 </a>
-                <a href="#tecnologias" class="btn-action">
-                    <p>Tecnologias</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-arrow-down" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                            d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1" />
+            </div>
+            <div class="arrow-tech">
+                <a href="#tecnologias" class="arrow">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
                     </svg>
                 </a>
             </div>
