2f52c259921a435086d50fa3461388e4fef14d46
Gustavo Raulino <gustavoraulinodasilva@gmail.com>
2024-10-15 21:23:59 -0300

    adicionado o sass ao meu projeto

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/sections/header.css',
                 'resources/css/sections/footer.css',
                 'resources/css/home.css',
+                'resources/sass/main.scss',
                 'resources/js/app.js'
             ],
             refresh: true,
